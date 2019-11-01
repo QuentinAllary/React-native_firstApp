210b9fbddd3a491ad8dfe6dd2602f9484a859a50
Pivil <remi.pontonnier.38@gmail.com>
2019-11-01 17:01:13 +0100

    add name

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button } from "react-native";
 
 import Chat from "./components/chat";
 import Menu from "./components/menu";
+import Home from "./components/home";
+
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: null,
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.menu}>
-          <Menu />
+          <Button
+            title="Chat"
+            onPress={() => this.setState({ currentPage: "chat" })}
+          />
         </View>
-        <View style={styles.chat}>
-          <Chat />
+        <View style={styles.content}>
+          {this.state.currentPage == "chat" ? <Chat /> : console.log("non")}
         </View>
       </View>
     );
@@ -30,5 +42,13 @@ const styles = StyleSheet.create({
   menu: {
     // backgroundColor: "skyblue",
     flex: 1
+  },
+  content: {
+    flex: 5
+  },
+  buttonContainer: {
+    flex: 1,
+    marginRight: 10,
+    marginLeft: 10
   }
 });
--- components/home.js
+++ components/home.js
@@ -1,36 +1,22 @@
 import React from "react";
-import { StyleSheet, Button, View} from "react-native";
+import { StyleSheet, TextInput, View, Button, Text } from "react-native";
 
 export default class Menu extends React.Component {
   render() {
     return (
       <>
-        <View style={styles.container}>
-          <View style={styles.buttonContainer}>
-            <Button title="Chat" />
-          </View>
-          <View style={styles.buttonContainer}>
-            <Button title="Video" />
-          </View>
-          <View style={styles.buttonContainer}>
-            <Button title="Files" />
-          </View>
+        <View>
+          <Text>Enter your name</Text>
+          <TextInput
+            style={{ borderColor: "black", borderWidth: 1 }}
+            onChangeText={text => this.setState({ name: text })}
+          ></TextInput>
+          <Button
+            title="Go to chat!"
+            onPress={() => navigate("Chat", { name: this.state.name })}
+          />
         </View>
       </>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  buttonContainer: {
-    flex: 1,
-    marginRight: 10,
-    marginLeft: 10
-  }
-});
