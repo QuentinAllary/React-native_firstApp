17f2f2f2b93d1fcdb7ed12cfcd5abc828853f640
Pivil <remi.pontonnier.38@gmail.com>
2019-11-02 14:27:21 +0100

    all is on fire

--- components/video.js
+++ components/video.js
@@ -1,64 +1,40 @@
 import React from "react";
 import { StyleSheet, View, Button, Text } from "react-native";
-import { RTCView, mediaDevices } from "react-native-webrtc";
 import AwesomeButtonRick from "react-native-really-awesome-button/src/themes/rick";
+import { RNCamera, FaceDetector } from "react-native-camera";
 
 export default class Video extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      localVideoURL: null
-    };
-  }
-
-  getLocalVideo() {
-    let isFront = true;
-    mediaDevices.enumerateDevices().then(sourceInfos => {
-      console.log(sourceInfos);
-      let videoSourceId;
-      for (let i = 0; i < sourceInfos.length; i++) {
-        const sourceInfo = sourceInfos[i];
-        if (
-          sourceInfo.kind == "videoinput" &&
-          sourceInfo.facing == (isFront ? "front" : "back")
-        ) {
-          videoSourceId = sourceInfo.deviceId;
-        }
-      }
-      mediaDevices
-        .getUserMedia({
-          audio: true,
-          video: {
-            mandatory: {
-              minWidth: 500, // Provide your own width, height and frame rate here
-              minHeight: 300,
-              minFrameRate: 30
-            },
-            facingMode: isFront ? "user" : "environment",
-            optional: videoSourceId ? [{ sourceId: videoSourceId }] : []
-          }
-        })
-        .then(stream => {
-          this.setState({ localVideoURL: stream.toURL() });
-        })
-        .catch(error => {
-          // Log error
-        });
-    });
-  }
   render() {
     return (
       <>
         <View style={styles.container}>
           <Text style={styles.headerStyle}>Video</Text>
           <AwesomeButtonRick
-                type="secondary"
-                onPress={() => this.getLocalVideo()}
-              >
-                Test
-              </AwesomeButtonRick>
-          <RTCView streamURL={this.state.localVideoURL} />
-          
+            type="secondary"
+            onPress={() => this.getLocalVideo()}
+          >
+            Test
+          </AwesomeButtonRick>
+          <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            androidCameraPermissionOptions={{
+              title: "Permission to use camera",
+              message: "We need your permission to use your camera",
+              buttonPositive: "Ok",
+              buttonNegative: "Cancel"
+            }}
+            androidRecordAudioPermissionOptions={{
+              title: "Permission to use audio recording",
+              message: "We need your permission to use your audio",
+              buttonPositive: "Ok",
+              buttonNegative: "Cancel"
+            }}
+          />
         </View>
       </>
     );
